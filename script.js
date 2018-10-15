23f1c46ff2a58e188a36b0a3d16ef67472d3cecf
Amber <32441978+Amber189@users.noreply.github.com>
2018-10-14 20:33:29 -0500

    add pages

---
+++ script.js
@@ -0,0 +1,9 @@
+
+let li = document.createElement("LI");
+   // Create a <li> node
+let textnode = document.createTextNode("testing");
+  // Create a text node
+li.appendChild(textnode);
+ // Append the text to <li>
+document.getElementsById("image-directory").appendChild(li);
+// Append <li> to <ul> with id="myList"
